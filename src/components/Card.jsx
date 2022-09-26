385028f24bbcc9d3a0962ed66f2b9fd181cf0b99
JCCiasico <carlociasico01@gmail.com>
2022-09-26 15:13:11 +0800

    chore: fix card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,10 +9,19 @@ export default function Card(){
                 }
             }>
             </div>
-            <div className="card--location">
-                <img src="../../src/assets/fill 219.png" alt="" className='icon-location'/>
-                <p className='card--country'>JAPAN</p>
-                <a href="#"  className='card--link'>View on Google Maps</a>
+            <div className='card--details'>
+                <div className="card--location">
+                    <img src="../../src/assets/fill 219.png" alt="" className='icon-location'/>
+                    <p className='card--country'>JAPAN</p>
+                    <a href="#"  className='card--link'>View on Google Maps</a>
+                </div>
+                <h2 className='details--title'>Mount Fuji</h2>
+                <p className='details--date'>12 Jan,2021 - 24 Jan,2021</p>
+                <p className='details--des'>Mount Fuji is the tallest mountain 
+                in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is 
+                the single most popular tourist site in Japan, for both Japanese 
+                and foreign tourists.
+                </p>
             </div>
         </div>
     )
