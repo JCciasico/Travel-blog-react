1e901f94b553812a5d5555fecef7ce2cf7f2591e
JCCiasico <carlociasico01@gmail.com>
2022-09-26 14:21:31 +0800

    travel blog website

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header(){
+    return(
+        <header>
+            <img src="../../src/assets/Fill 213.png" alt="logo" />
+            <p>my travel journal.</p>
+        </header>
+    )
+}
