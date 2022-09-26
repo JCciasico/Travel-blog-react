57730e3d0f6da9bc27cbb63bc5c41365d3c5b912
JCCiasico <carlociasico01@gmail.com>
2022-09-26 15:35:29 +0800

    chore: fixed data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
 import Header from './components/Header'
 import Card from './components/Card'
+import Traveldata from './data/Traveldata'
 
 function App() {
+  const travel = Traveldata.map(travel => {
+    return <Card
+            key = {travel.id}
+            travel = {travel}
+          />
+  })
+
   return (
     <div className="App">
       <Header />
       <div className="card-container">
-          <Card/>
+          {travel}
       </div>
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,18 @@
 import './Card.css'
 import background from '../../src/assets/mount-fuji.png'
-export default function Card(){
+export default function Card(props){
     return(
         <div className="card">
-            <div className='image' style={
-                {
-                    backgroundImage: `url(${background})`
-                }
-            }>
-            </div>
+            <img src={`../../src/assets/${props.travel.imageUrl}`} className="card--image" alt="" />
             <div className='card--details'>
                 <div className="card--location">
                     <img src="../../src/assets/fill 219.png" alt="" className='icon-location'/>
-                    <p className='card--country'>JAPAN</p>
+                    <p className='card--country'>{props.travel.location}</p>
                     <a href="#"  className='card--link'>View on Google Maps</a>
                 </div>
-                <h2 className='details--title'>Mount Fuji</h2>
-                <p className='details--date'>12 Jan,2021 - 24 Jan,2021</p>
-                <p className='details--des'>Mount Fuji is the tallest mountain 
-                in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is 
-                the single most popular tourist site in Japan, for both Japanese 
-                and foreign tourists.
+                <h2 className='details--title'>{props.travel.title}</h2>
+                <p className='details--date'>{props.travel.startDate} - {props.travel.endDate}</p>
+                <p className='details--des'>{props.travel.description}
                 </p>
             </div>
         </div>
